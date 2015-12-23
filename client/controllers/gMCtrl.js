f4513ba4e80f0a8e1713148ec19bb89220d1c9dc
Rainer Paskeiwicz <rainer.paskiewicz@gmail.com>
2015-12-23 18:21:03 -0500

    Styling and base frame
    -Base styling of the game list
    -collections for the gm view.

--- client/controllers/gMCtrl.js
+++ client/controllers/gMCtrl.js
@@ -1,4 +1,38 @@
-angular.module('player-tracker').controller('GMCtrl', ['$scope', '$stateParams', '$meteor',
-	function ($scope, $stateParams, $meteor) {
+angular.module('player-tracker').controller('GMCtrl', ['$scope', '$stateParams', '$meteor', '$location', '$reactive',
+	function ($scope, $stateParams, $meteor, $location, $reactive) {
+		$reactive(this).attach($scope);
+		$scope.url = $location.absUrl();
+		$scope.gameId = $stateParams.gameId;
+		$scope.subscribe("players", () => [$scope.gameId]);
+		$scope.subscribe("game", () => [$scope.gameId]);
+		$scope.subscribe("playerRecord", () => [Meteor.userId()]);
+
+		$scope.showConditons = false;
+		$scope.helpers({
+			game(){
+				return Games.findOne({_id:$scope.gameId});
+			},
+			players(){
+				return Players.find(
+					{game : $scope.gameId },
+					{sort:{ 'battle.round' : 1, 'battle.init': -1 }});
+			},
+			playerRecord(){
+					return Players.findOne({game: $scope.gameId, owner: Meteor.userId()});
+			}
+		});
+		console.log($scope.players);
+		// $scope.autorun(function(){
+		// 	$scope.pageTitle = $scope.game.name;
+		// })
+		$scope.autorun(() => {
+			// console.log(`current search string is: `, $scope.game.name);
+		});
+		$scope.$watch("game.name", function(newValue, oldValue){
+
+			if(newValue){
+				$scope.pageTitle = $scope.game.name;
+			}
+		});
 
 }]);
